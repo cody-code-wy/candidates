2ab8d22dd0916f78619056280a4830b5722cd0b6
William Young <william@werl.me>
2016-05-26 18:30:20 -0700

    Gist

--- filters.rb
+++ filters.rb
@@ -1,11 +1,33 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
 
-def find(id)
+require './candidates.rb'
+require 'active_support/all'
 
+def find(id)
+  @candidates.detect do |c|
+    c[:id] == id
+  end
 end
 
 def experienced?(candidate)
+  candidate[:years_of_experience] >= 2
 end
 
+def github_points?(candidate)
 # More methods will go below
+
+def qualified_candidates(candidates)
+  candidates.select do |c|
+    #Experienced
+    xp = experienced? c
+    #Github Points
+    gp = c[:github_points] >= 100
+    #Age
+    ag = c[:age] >= 18
+    #Ruby or Python
+    rp = c[:languages].any? { |l| l == "Ruby" || l == "Python" }
+    #Days ago
+    da = c[:date_applied] >= 15.day.ago.to_date
+  end
+end
